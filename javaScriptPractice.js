53c5b9896bd134d0605a8915c0770624bfb47512
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2022-12-29 21:55:25 +0000

    start exercise 6

--- javaScriptPractice.js
+++ javaScriptPractice.js
@@ -112,3 +112,29 @@ const addNew2 = (str) => (str.indexof("New!") === 0 ? str : `New! ${str}`);
 
 addNew("adidas");
 addNew("New! nike");
+
+/* --------------------------------------------------------
+Exercise 6:
+--------------
+100 doors in a row are all initially closed. You make 100 passes by the doors. The first time through, you visit every door and toggle the door (if the door is closed, you open it; if it is open, you close it).
+The second time you only visit every 2nd door (door #2, #4, #6, ...).
+The third time, every 3rd door (door #3, #6, #9, ...), etc, until you only visit the 100th door.
+
+Question: What state are the doors in after the last pass? Which are open, which are closed?
+
+[Source http://rosettacode.org]
+*/
+
+let hallway = new Array(100).fill("closed"); // Creating an array of size 100 and filled of 'closed'
+console.log(hallway);
+
+for(let i = 1; i <= hallway.length; i++) {
+  hallway[i] = 
+}
+// 1st pass: toggle all doors
+// 2nd pass: toggle every 2nd door (door #2, #4, #6, ...).
+// 3rd pass: toggle every 3rd door (door #3, #6, #9, ...)
+// ...
+// ...
+// ...
+// 100th pass: only toggle the 100th door
