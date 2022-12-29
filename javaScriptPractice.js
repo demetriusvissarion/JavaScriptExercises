68ab0fca81307f4269c5e605cd4e7969fd69b699
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2022-12-29 21:56:46 +0000

    hide olds

--- javaScriptPractice.js
+++ javaScriptPractice.js
@@ -4,36 +4,36 @@ Write a JavaScript program to check two numbers and return true if one of the nu
 */
 
 ////// my solution
-function checkOneHundred(a, b) {
-  if (a === 100 || b === 100 || a + b === 100) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function checkOneHundred(a, b) {
+//   if (a === 100 || b === 100 || a + b === 100) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-checkOneHundred(47, 53);
-checkOneHundred(100, 53);
-checkOneHundred(47, 100);
-checkOneHundred(47, 58);
+// checkOneHundred(47, 53);
+// checkOneHundred(100, 53);
+// checkOneHundred(47, 100);
+// checkOneHundred(47, 58);
 
 /////// teacher solution
-const checkIf100 = (a, b) => a === 100 || b === 100 || a + b === 100;
-checkIf100(47, 53);
-checkIf100(100, 53);
-checkIf100(47, 100);
-checkIf100(47, 58);
+// const checkIf100 = (a, b) => a === 100 || b === 100 || a + b === 100;
+// checkIf100(47, 53);
+// checkIf100(100, 53);
+// checkIf100(47, 100);
+// checkIf100(47, 58);
 
 /* --------------------------------------------------------
 Exercise 2:
 --------------
 Write a JavaScript program to get the extension of a filename.
 */
-const getFileExtension = (file) =>
-  `File has the following extension: ${file.slice(file.lastIndexOf(".") + 1)}`;
+// const getFileExtension = (file) =>
+//   `File has the following extension: ${file.slice(file.lastIndexOf(".") + 1)}`;
 
-getFileExtension("text.txt");
-getFileExtension("webpack.config.js");
+// getFileExtension("text.txt");
+// getFileExtension("webpack.config.js");
 
 /* --------------------------------------------------------
 Exercise 3:
@@ -42,33 +42,33 @@ Write a JavaScript program to replace every character in a given string with the
 */
 
 //////// my solution
-const replaceLetterWithNext = (str) => {
-  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
-    ""
-  );
-  let array = str.split(" ");
-  let result = [];
-  for (item of array) {
-    let convertedWord = item
-      .toString()
-      .split("")
-      .map((x) => alphabet[alphabet.indexOf(x) + 1])
-      .join("");
-    result.push(convertedWord);
-  }
-  return result.join(" ");
-};
+// const replaceLetterWithNext = (str) => {
+//   let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
+//     ""
+//   );
+//   let array = str.split(" ");
+//   let result = [];
+//   for (item of array) {
+//     let convertedWord = item
+//       .toString()
+//       .split("")
+//       .map((x) => alphabet[alphabet.indexOf(x) + 1])
+//       .join("");
+//     result.push(convertedWord);
+//   }
+//   return result.join(" ");
+// };
 
-replaceLetterWithNext("This is a test");
+// replaceLetterWithNext("This is a test");
 
 ///////// teacher solution
-const moveCharsForward = (str) =>
-  str
-    .split("")
-    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
-    .join("");
+// const moveCharsForward = (str) =>
+//   str
+//     .split("")
+//     .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+//     .join("");
 
-moveCharsForward("This is a test");
+// moveCharsForward("This is a test");
 
 /* --------------------------------------------------------
 Exercise 4:
@@ -79,21 +79,21 @@ mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 */
 
 //////// my solution
-const getCurrentDate = () => {
-  let date = new Date();
-  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-};
+// const getCurrentDate = () => {
+//   let date = new Date();
+//   return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+// };
 
-getCurrentDate();
+// getCurrentDate();
 
 //////// teacher solution
-const formatDate = (date = new Date()) => {
-  const days = date.getDate();
-  const months = date.getMonth() + 1;
-  const years = date.getFullYear();
-  return `${days}/${months}/${years}`;
-};
-formatDate();
+// const formatDate = (date = new Date()) => {
+//   const days = date.getDate();
+//   const months = date.getMonth() + 1;
+//   const years = date.getFullYear();
+//   return `${days}/${months}/${years}`;
+// };
+// formatDate();
 
 /* --------------------------------------------------------
 Exercise 5:
@@ -102,16 +102,16 @@ Write a JavaScript program to a new string adding "New!" in front of a given str
 */
 
 ///////// my solution
-const addNew = (str) => (str.substring(0, 4) === "New!" ? str : `New! ${str}`);
+// const addNew = (str) => (str.substring(0, 4) === "New!" ? str : `New! ${str}`);
 
-addNew("adidas");
-addNew("New! nike");
+// addNew("adidas");
+// addNew("New! nike");
 
-///////// teacher solution
-const addNew2 = (str) => (str.indexof("New!") === 0 ? str : `New! ${str}`);
+// ///////// teacher solution
+// const addNew2 = (str) => (str.indexof("New!") === 0 ? str : `New! ${str}`);
 
-addNew("adidas");
-addNew("New! nike");
+// addNew("adidas");
+// addNew("New! nike");
 
 /* --------------------------------------------------------
 Exercise 6:
