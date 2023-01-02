c0984db07d9bfc22c1ffef41b358205373aeacd6
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-02 22:23:46 +0000

    added one hundred doors

--- onHundredDoors.js
+++ onHundredDoors.js
@@ -6,3 +6,21 @@ The third time, every 3rd door (door #3, #6, #9, ...), etc, until you only visit
 Question: What state are the doors in after the last pass? Which are open, which are closed?
 
 [Source http://rosettacode.org] */
+
+// my solution
+const getDoorStates = (numberOfDoors) => {
+  // generate array
+  let array = new Array(numberOfDoors).fill("closed");
+  for (let i = 1; i < array.length + 1; i++) {
+    // check if square of index of item is smaller than lenght of array, if it is change it's value to 'open'
+    if (Math.pow(i, 2) <= array.length) {
+      array[Math.pow(i, 2) - 1] = "open";
+    }
+    // add numbers to doors for readability
+    array[i - 1] = `Door ${i} is: ${array[i - 1]}`;
+  }
+  let result = array.join(",  ");
+  return result;
+};
+
+getDoorStates(100);
