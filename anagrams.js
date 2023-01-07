0735111ce7b59e00c7bccebcceb1c6034a87e85d
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-07 18:39:55 +0000

    anagram

--- anagrams.js
+++ anagrams.js
@@ -1,3 +1,4 @@
+// 1.
 // Write a program to generate all potential anagrams of an input string.
 
 // For example, the potential anagrams of "biro" are
@@ -8,3 +9,36 @@
 // obir obri oibr oirb orbi orib
 
 // my solution:
+
+// 2.
+// DESCRIPTION:
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+// my solution:
+let isAnagram = function (test, original) {
+  test = test.toLowerCase().split("").sort().join("");
+  original = original.toLowerCase().split("").sort().join("");
+
+  if (test === original) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// tests:
+isAnagram("foefet", "toffee"); // should output: 'The word "foefet" is an anagram of "toffee"'
+isAnagram("Twoo", "WooT"); // should ooutput: 'The word "Twoo" is an anagram of "WooT"'
+
+isAnagram("dumble", "bumble"); // should ooutput: 'Characters do not match for test case "dumble", "bumble"'
+isAnagram("ound", "round"); // should ooutput: 'Missing characters for test case "ound", "round"'
+isAnagram("apple", "pale"); // should ooutput: 'Same letters, but different count'
